7bea5cc98ec2846df6d04fef98ae6af95f7d615f
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-07 22:43:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,11 +33,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    result = self.find_by_name
+    result = self.find_by_name(name)
     if result
       result
     else
-      self.create_by_name
+      self.create_by_name(name)
     end
   end
 end
