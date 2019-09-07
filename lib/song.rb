a19f649acd524ffd62d813555df50d68610c3a49
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-07 21:59:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,17 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    @@all.find{ |e| e.name == name }
+  end
+
+  def self.create_by_name(name)
+
+  end
 end
